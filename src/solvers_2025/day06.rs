74e4f97bfa8017218c33dc10fdda9e3f541d71df
vaisest <4550061+vaisest@users.noreply.github.com>
2025-12-06 21:29:42 +0200

    fix day 6

--- src/solvers_2025/day06.rs
+++ src/solvers_2025/day06.rs
@@ -1,24 +1,54 @@
-use arrayvec::ArrayVec;
-
 pub fn part1(input: String) -> String {
-    let mut problems: Vec<ArrayVec<&str, 5>> = vec![];
-    for (i, line) in input.lines().enumerate() {
-        for (j, word) in line.split(" ").filter(|v| !v.is_empty()).enumerate() {
-            if i == 0 {
-                problems.push(ArrayVec::new());
-            }
-            problems[j].push(word);
-        }
-    }
+    let input: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+
+    let width = input[0].len();
+    // actual input is 1 more row than test
+    let height = input.len();
+    assert!(
+        input.iter().all(|line| line.len() == width),
+        "malformed input"
+    );
 
     let mut total = 0;
-    for problem in problems {
-        let numbers = problem[..problem.len() - 1]
-            .iter()
-            .map(|v| v.parse::<u64>().unwrap());
-        match *problem.last().unwrap() {
-            "*" => total += numbers.reduce(|a, b| a * b).unwrap(),
-            "+" => total += numbers.reduce(|a, b| a + b).unwrap(),
+    // solved similar to part 2
+    for (col_idx, operator) in input[height - 1]
+        .iter()
+        .enumerate()
+        .filter(|v| !v.1.is_ascii_whitespace())
+    {
+        let mut col_width = 0;
+        for idx_elem in &input[height - 1][(col_idx + 1)..] {
+            if idx_elem.is_whitespace() {
+                col_width += 1;
+            } else {
+                break;
+            }
+        }
+        // edge case: right edge
+        if col_width + col_idx == width - 1 {
+            col_width += 1;
+        }
+
+        let mut numbers = Vec::new();
+        // this part differs from p2. we just take numbers like a human would
+        // instead of the right to left column stuff
+        for row in input.iter().take(height - 1) {
+            let number = row[col_idx..col_idx + col_width]
+                .iter()
+                .filter_map(|c| {
+                    if c.is_ascii_whitespace() {
+                        None
+                    } else {
+                        Some(c.to_digit(10).unwrap() as u64)
+                    }
+                })
+                .reduce(|a, b| a * 10 + b)
+                .unwrap();
+            numbers.push(number);
+        }
+        match operator {
+            '*' => total += numbers.into_iter().reduce(|a, b| a * b).unwrap(),
+            '+' => total += numbers.into_iter().reduce(|a, b| a + b).unwrap(),
             _ => unreachable!(),
         }
     }
